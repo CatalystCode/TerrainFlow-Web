e730b1bc8e9d958155c9660002e2c2510097c94c
Stefan Gordon <stefan@stefangordon.com>
2016-03-09 10:26:59 -0800

    Move from mvccore to mvc.

--- src/TerrainFlow/Startup.cs
+++ src/TerrainFlow/Startup.cs
@@ -38,7 +38,7 @@ namespace TerrainFlow
             services.AddAuthentication(options => options.SignInScheme = "Cookie");
 
             // Add MVC services to the services container.
-            services.AddMvcCore();
+            services.AddMvc();
 
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
